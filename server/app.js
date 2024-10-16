4e1eaa6355f0ac50533258f1996795b6c87350e9
SanketBhandarii <bhandarisanketp@gmail.com>
2024-10-16 17:48:51 +0530

    Updates

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ app.use(
     credentials: true,
   })
 );
-
+app.set("trust proxy", true);
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
--- server/index.js
+++ server/index.js
@@ -11,6 +11,9 @@ connectDB()
     app.listen(process.env.PORT || 8080, () => {
       console.log(`Server is running at port : ${process.env.PORT}`);
     });
+    app.get("/", (req, res) =>{
+      res.send("<h1>Hi! Welcome to my backend</h1>")
+    })
   })
   .catch((err) => {
     console.log("MONGODB connection failed");
